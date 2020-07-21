5a87297a942a48cd3cdeac1ab8b9a584c37d3aa5
dokimphuong <dokimphuong1999@gmail.com>
2020-07-21 10:29:22 +0700

    create project

---
+++ src/controller/DangKyController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/dangky")
+public class DangKyController extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher rd = getServletContext().getRequestDispatcher("/register.jsp");
+        rd.forward(request, response);
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+}
